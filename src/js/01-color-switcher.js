8b4fa381c8c5ae427270bc57cf4ba10c74096cd2
tinerr <tinerr02@gmail.com>
2023-12-22 12:31:44 +0800

    Add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  buttonDisabled(startBtn, stopBtn);
+  timerId = setInterval(() => {
+    return (document.body.style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777215
+    )
+      .toString(16)
+      .padStart(6, 0)}`);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  buttonDisabled(stopBtn, startBtn);
+  clearInterval(timerId);
+});
+buttonDisabled(stopBtn, startBtn);
+function buttonDisabled(on, off) {
+  on.disabled = true;
+  off.disabled = false;
+}
